810b79c43bf06b9998f19f213973a5b247804a99
huiyuxie <huiyuxie.sde@gmail.com>
2023-05-17 19:22:16 +0000

    more test

--- test.jl
+++ test.jl
@@ -1,4 +1,11 @@
 # This file is for testing aws nvidia gpu
-using CUDA
-println("Hello World!")
+using CUDA, Test
 CUDA.versioninfo()
+
+N = 2^20
+
+x_d = CUDA.fill(1.0f0, N)  # a vector stored on the GPU filled with 1.0 (Float32)
+y_d = CUDA.fill(2.0f0, N)  # a vector stored on the GPU filled with 2.0
+
+y_d .+= x_d
+@test all(Array(y_d) .== 3.0f0)

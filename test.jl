850912fa6cd8194c48a0fdae16c0ef42c1139c13
huiyuxie <huiyuxie.sde@gmail.com>
2023-05-18 23:33:34 +0000

    initial test

--- test.jl
+++ test.jl
@@ -1,11 +1,36 @@
-# This file is for testing aws nvidia gpu
-using CUDA, Test
-CUDA.versioninfo()
+### This file is for testing aws nvidia gpu
 
-N = 2^20
+using Trixi, LinearAlgebra, OrdinaryDiffEq, CUDA, Test
 
-x_d = CUDA.fill(1.0f0, N)  # a vector stored on the GPU filled with 1.0 (Float32)
-y_d = CUDA.fill(2.0f0, N)  # a vector stored on the GPU filled with 2.0
+coordinates_min = -1.0
+coordinates_max = 1.0
+n_elements      = 16
+polydeg         = 3
+dx              = (coordinates_max - coordinates_min) / n_elements
 
-y_d .+= x_d
-@test all(Array(y_d) .== 3.0f0)
+basis = LobattoLegendreBasis(polydeg)
+
+nodes = CuArray{Float32}(basis.nodes)
+
+x_l = CuArray{Float32}(collect(0:n_elements-1)) .* Float32(dx) + CuArray{Float32}(fill(dx / 2 - 1, n_elements))
+
+x = CuArray{Float32}(fill(1.0, polydeg + 1)) * transpose(x_l) + nodes * transpose(CuArray{Float32}(fill(1, n_elements))) .* (Float32(dx) / 2)
+
+function initial_condition_sine_wave_gpu!(u0, x)
+	index = threadIdx().x
+	stride = blockDim().x
+	for i ∈ index:stride:size(x)[1]
+		@inbounds u0[:, i] = x[:, i]
+	end
+	return nothing
+end
+
+u0 = similar(x)
+@cuda threads = 256 initial_condition_sine_wave_gpu!(u0, x)
+println(u0[:, 15])
+#= println(typeof(x)) 
+u0 = similar(x)
+println(u0)
+@cuda threads = 256 initial_condition_sine_wave_gpu!(u0, x)
+print(u0)
+=#

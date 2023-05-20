d8bd048a8cd46fd481f4aacb55498f430e42853d
huiyuxie <huiyuxie.sde@gmail.com>
2023-05-20 03:03:20 +0000

    test kernels

---
+++ sample_kernels.jl
@@ -0,0 +1,159 @@
+### This is a test file for running CUDA sample kernels, which use different combinations 
+### of 1, 2, and 3 dimensions for both blocks and grids to conduct computing on 1D, 2D, and 
+### 3D arrays.
+### Note that not all the combinations are implemented here due to the similarity.
+
+using CUDA, Test
+
+#--------------------------------------------------------------------- Kernels for 1D array
+# In total there are 3 choices for 1D array computing: 
+# 1) 1D block; 2) 1D grid; 2) 1D block, 1D grid
+
+# 1D block 
+function gpu_add1!(y, x)
+	index = threadIdx().x
+	stride = blockDim().x
+
+	for i ∈ index:stride:length(y)
+		@inbounds y[i] += x[i]
+	end
+
+	return nothing
+end
+
+N = 2^20
+x = CUDA.fill(1.0f0, N)
+y = CUDA.fill(2.0f0, N)
+@cuda threads = 256 gpu_add1!(y, x)
+@test all(Array(y) .== 3.0f0)
+
+# 1D grid
+function gpu_add2!(y, x)
+	index = blockIdx().x
+	stride = gridDim().x
+
+	for i ∈ index:stride:length(y)
+		@inbounds y[i] += x[i]
+	end
+
+	return nothing
+end
+
+N = 2^20
+x = CUDA.fill(1.0f0, N)
+y = CUDA.fill(2.0f0, N)
+@cuda blocks = 256 gpu_add2!(y, x)
+@test all(Array(y) .== 3.0f0)
+
+# 1D block, 1D grid
+function gpu_add3!(y, x)
+	index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+	stride = gridDim().x * blockDim().x
+
+	for i ∈ index:stride:length(y)
+		@inbounds y[i] += x[i]
+	end
+
+	return nothing
+end
+
+N = 2^20
+x = CUDA.fill(1.0f0, N)
+y = CUDA.fill(2.0f0, N)
+numblocks = ceil(Int, N / 256)
+@cuda threads = 256 blocks = numblocks gpu_add3!(y, x)
+@test all(Array(y) .== 3.0f0)
+
+#--------------------------------------------------------------------- Kernels for 2D array
+# In total there are 5 choices for 2D array computing:
+# 1) 2D block; 2) 2D grid; 3) 2D block, 1D grid; 4) 1D block, 2D grid; 5) 2D block, 2D grid
+
+# 2D block
+function gpu_add4!(y, x)
+	index_i = threadIdx().x
+	index_j = threadIdx().y
+
+	stride_i = blockDim().x
+	stride_j = blockDim().y
+
+	for i ∈ index_i:stride_i:size(y, 1)
+		for j ∈ index_j:stride_j:size(y, 2)
+			@inbounds y[i, j] += x[i, j]
+		end
+	end
+
+	return nothing
+end
+
+N = 2^10
+x = CUDA.ones(N, N)
+y = CUDA.zeros(N, N)
+@cuda threads = (16, 16) gpu_add4!(y, x)
+@test all(Array(y) .== 1.0f0)
+
+# 2D block, 1D grid
+function gpu_add5!(y, x)
+	index_i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+	index_j = threadIdx().y
+
+	stride_i = gridDim().x * blockDim().x
+	stride_j = blockDim().y
+
+	for i ∈ index_i:stride_i:size(y, 1)
+		for j ∈ index_j:stride_j:size(y, 2)
+			@inbounds y[i, j] += x[i, j]
+		end
+	end
+
+	return nothing
+end
+
+N = 2^10
+x = CUDA.ones(N, N)
+y = CUDA.zeros(N, N)
+numblocks = ceil(Int, N / 256)
+@cuda threads = (16, 16) blocks = numblocks gpu_add5!(y, x)
+@test all(Array(y) .== 1.0f0)
+
+# 2D block, 2D grid
+function gpu_add6!(y, x)
+	index_i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+	index_j = (blockIdx().y - 1) * blockDim().y + threadIdx().y
+
+	stride_i = gridDim().x * blockDim().x
+	stride_j = gridDim().y * blockDim().y
+
+	for i ∈ index_i:stride_i:size(y, 1)
+		for j ∈ index_j:stride_j:size(y, 2)
+			@inbounds y[i, j] += x[i, j]
+		end
+	end
+
+	return nothing
+end
+
+N = 2^10
+x = CUDA.ones(N, N)
+y = CUDA.zeros(N, N)
+numblocks = ceil(Int, N / 256)
+@cuda threads = (16, 16) blocks = (numblocks, numblocks) gpu_add6!(y, x)
+@test all(Array(y) .== 1.0f0)
+
+#--------------------------------------------------------------------- Kernels for 3D array
+# In total there are 7 choices for 3D array computing:
+# 1) 3D block; 2) 3D grid; 3) 3D block, 1D grid; 4) 1D block, 3D grid; 5) 3D block, 2D grid;
+# 6) 2D block, 3D grid; 7) 3D block, 3D grid
+
+function gpu_add!(y, x)
+	i = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+	j = (blockIdx().y - 1) * blockDim().y + threadIdx().y
+	k = (blockIdx().z - 1) * blockDim().z + threadIdx().z
+
+	y[i, j, k] += x[i, j, k]
+
+	return nothing
+end
+N = 32
+x = CUDA.ones(N, N, N)
+y = CUDA.zeros(N, N, N)
+@cuda threads = (16, 16, 16) blocks = (2, 2, 2) gpu_add!(y, x)
--- test.jl
+++ test.jl
@@ -16,19 +16,32 @@ x_l = CuArray{Float32}(collect(0:n_elements-1)) .* Float32(dx) + CuArray{Float32
 
 x = CuArray{Float32}(fill(1.0, polydeg + 1)) * transpose(x_l) + nodes * transpose(CuArray{Float32}(fill(1, n_elements))) .* (Float32(dx) / 2)
 
-function initial_condition_sine_wave_gpu!(u0, x)
-	index = threadIdx().x
-	stride = blockDim().x
-	for i ∈ index:stride:size(x)[1]
-		@inbounds u0[:, i] = x[:, i]
+u0 = map(x -> Float32(0.5) * sin(pi * x) + 1, x)
+
+function apply_surface_flux!(γ, u1, u2)
+	# Get thread index 
+	index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+
+	# Get loop stride
+	stride = gridDim().x * blockDim().x
+
+	# Choose to use Lax-Friedrichs flux and set equations
+	surface_flux = flux_lax_friedrichs
+	equations = LinearScalarAdvectionEquation1D(1.0)
+
+	# Assign each thread to calculate corresponding flux
+	for i ∈ index:stride:n_elements
+		@inbounds γ[i] = surface_flux(u1[i], u2[i], 1, equations)
 	end
+
 	return nothing
 end
 
-u0 = similar(x)
-@cuda threads = 256 initial_condition_sine_wave_gpu!(u0, x)
-println(u0[:, 15])
-#= println(typeof(x)) 
+
+#=  
+
+
+
 u0 = similar(x)
 println(u0)
 @cuda threads = 256 initial_condition_sine_wave_gpu!(u0, x)

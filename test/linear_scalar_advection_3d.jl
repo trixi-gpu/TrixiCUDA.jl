43d829ba878cfd81a00334e50c12195c9e0fa3e8
Huiyu Xie <huiyuxie.sde@gmail.com>
2023-07-04 15:08:12 -1000

    add 3d

---
+++ test/linear_scalar_advection_3d.jl
@@ -0,0 +1,23 @@
+# The header part of test
+Random.seed!(12345)
+
+advection_velocity = (0.2, -0.7, 0.5)
+equations = LinearScalarAdvectionEquation3D(advection_velocity)
+
+solver = DGSEM(polydeg=3, surface_flux=flux_lax_friedrichs)
+
+coordinates_min = (-1.0, -1.0, -1.0)
+coordinates_max = (1.0, 1.0, 1.0)
+
+mesh = TreeMesh(coordinates_min, coordinates_max, initial_refinement_level=3, n_cells_max=30_000)
+
+semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition_convergence_test, solver)
+
+@unpack mesh, equations, initial_condition, boundary_conditions, source_terms, solver, cache = semi
+
+t = 0.0
+l = nvariables(equations) * nnodes(solver)^ndims(mesh) * nelements(solver, cache)
+du_ode = rand(Float64, l)
+u_ode = rand(Float64, l)
+du = wrap_array(du_ode, mesh, equations, solver, cache)
+u = wrap_array(u_ode, mesh, equations, solver, cache)

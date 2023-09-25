0b312b22e82b1ed2122f82bac8a7e0fcb3fa6d06
huiyuxie <huiyuxie.sde@gmail.com>
2023-09-25 21:24:00 +0000

    kernel tests

--- cuda_dg_1d.cu
+++ cuda_dg_1d.cu
@@ -9,6 +9,9 @@
 // Using namespaces
 using namespace std;
 
+// Kernel configurators 
+//----------------------------------------------
+
 // CUDA kernel configurator for 1D array computing
 pair<dim3, dim3> configurator_1d(void* kernelFun, int arrayLength) {
     cudaFuncAttributes attributes;
@@ -44,6 +47,37 @@ pair<dim3, dim3> configurator_3d(void* kernelFun, int arrayWidth, int arrayHeigh
     dim3 blocks(ceil(static_cast<float>(arrayWidth) / threads.x), ceil(static_cast<float>(arrayHeight) / threads.y), ceil(static_cast<float>(arrayDepth) / threads.z));
 
     return {threads, blocks};
+}
+
+// CUDA kernels
+//----------------------------------------------
+
+// Copy data from host to device (from double to float)
+void copy_to_gpu(float*** du_device, const double*** du_host, float*** u_device, const double*** u_host, int dimX, int dimY, int dimZ) {
+    int totalElements = dimX * dimY * dimZ;
+
+    // Allocate memory on the GPU
+    cudaMalloc(&du_device, totalElements * sizeof(float));
+    cudaMalloc(&u_device, totalElements * sizeof(float));
+
+    // Set du_device to zeros
+    cudaMemset(*du_device, 0, totalElements * sizeof(float));
+
+    // Convert double data on the CPU to float and then transfer to the GPU
+    float* temp_u_float = new float[totalElements];
+
+    int idx = 0;
+    for (int i = 0; i < dimX; i++) {
+        for (int j = 0; j < dimY; j++) {
+            for (int k = 0; k < dimZ; k++) {
+                temp_u_float[idx++] = static_cast<float>(u_host[i][j][k]);
+            }
+        }
+    }
+
+    cudaMemcpy(*u_device, temp_u_float, totalElements * sizeof(float), cudaMemcpyHostToDevice);
+
+    delete[] temp_u_float;
 }
 
 
---
+++ kernel_tests/kernel_test2.cu
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <cmath>
+
+// Host code
+void copy_to_gpu(float*** &du_device, double*** du_host, float*** &u_device, double*** u_host, int width, int height, int depth) {
+    
+    // 3D extent for allocation
+    cudaExtent extent = make_cudaExtent(width * sizeof(float), height, depth);
+    
+    // Allocate memory for du on the GPU and set to zero
+    cudaPitchedPtr devDuPitchedPtr;
+    cudaMalloc3D(&devDuPitchedPtr, extent);
+    cudaMemset3D(devDuPitchedPtr, 0, extent);
+    
+    // Allocate memory for u on the GPU
+    cudaPitchedPtr devUPitchedPtr;
+    cudaMalloc3D(&devUPitchedPtr, extent);
+    
+    // Convert u from double to float and copy to GPU
+    cudaMemcpy3DParms copyParams = {0};
+    float* temp_u_float = new float[width * height * depth];
+    
+    int idx = 0;
+    for (int z = 0; z < depth; z++) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                temp_u_float[idx++] = static_cast<float>(u_host[z][y][x]);
+            }
+        }
+    }
+
+    copyParams.srcPtr   = make_cudaPitchedPtr((void*)temp_u_float, width * sizeof(float), width, height);
+    copyParams.dstPtr   = devUPitchedPtr;
+    copyParams.extent   = extent;
+    copyParams.kind     = cudaMemcpyHostToDevice;
+    cudaMemcpy3D(&copyParams);
+
+    // Assign the pointers (You might need a way to handle these pointers outside this function)
+    du_device = (float***)devDuPitchedPtr.ptr;
+    u_device = (float***)devUPitchedPtr.ptr;
+    
+    delete[] temp_u_float;
+}
+
+
+bool areArraysEqual(float*** array1, float*** array2, int width, int height, int depth) {
+    for (int z = 0; z < depth; ++z) {
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                if (std::abs(array1[z][y][x] - array2[z][y][x]) > 1e-6) {  // using a small threshold for floating point comparison
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+void test_copy_to_gpu() {
+    const int width = 64, height = 64, depth = 64;
+
+    // Allocate and initialize host memory
+    double*** du_host = new double**[depth];
+    double*** u_host = new double**[depth];
+    for (int z = 0; z < depth; ++z) {
+        du_host[z] = new double*[height];
+        u_host[z] = new double*[height];
+        for (int y = 0; y < height; ++y) {
+            du_host[z][y] = new double[width];
+            u_host[z][y] = new double[width];
+            for (int x = 0; x < width; ++x) {
+                du_host[z][y][x] = 0.0; // This should be zeros on the GPU as well
+                u_host[z][y][x] = static_cast<double>(x + y + z); // Some arbitrary initialization for testing
+            }
+        }
+    }
+
+    // Pointers for device memory
+    float*** du_device = nullptr;
+    float*** u_device = nullptr;
+
+    // Call our function
+    copy_to_gpu(du_device, du_host, u_device, u_host, width, height, depth);
+
+    // Here you would typically copy back the data from the GPU to the host 
+    // and then compare to verify if the results are as expected.
+    // For the sake of this example, we will assume that there's a function 
+    // areArraysEqual that can compare two 3D arrays on the device.
+
+    // If this function existed, you would call:
+    // bool isEqual = areArraysEqual(du_device, expected_du_device, width, height, depth);
+    
+    // For now, let's print a message to know the function was called.
+    std::cout << "copy_to_gpu function was called. Verification functionality should be added." << std::endl;
+
+    // Cleanup host memory
+    for (int z = 0; z < depth; ++z) {
+        for (int y = 0; y < height; ++y) {
+            delete[] du_host[z][y];
+            delete[] u_host[z][y];
+        }
+        delete[] du_host[z];
+        delete[] u_host[z];
+    }
+    delete[] du_host;
+    delete[] u_host;
+
+    // Cleanup device memory
+    // cudaFree(du_device);  // Caution: This would be more complex with 3D memory allocations
+    // cudaFree(u_device);
+}
+
+int main() {
+    test_copy_to_gpu();
+    return 0;
+}

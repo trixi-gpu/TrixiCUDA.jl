14d44e7cad94715783b4c90e53652429842c65dc
huiyuxie <huiyuxie.sde@gmail.com>
2023-10-02 06:41:45 +0000

    configurators

--- cuda_cpp/configurator.h
+++ cuda_cpp/configurator.h
@@ -1,56 +1,55 @@
 /*
-Define kernel congifurators for parallel computing on 1D, 2D and 3D blocks and grids.
+Define kernel congifurators for parallel computing on 1D, 2D and 3D blocks and grids. Here the CUDA
+occupancy calculator is applied.
 */
 
 #ifndef CONFIGURATOR_H
 #define CONFIGURATOR_H
 
 // CUDA kernel configurator for parallel computing on 1D blocks and grids
-__host__ pair<dim3, dim3> configurator1D(void *kernelFun, int arrayLength) {
-    int blockSize;
-    int minGridSize;
-
-    cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize,
-                                       kernelFun); // Use CUDA occupancy calculator
-
-    int threads = blockSize;
-    int blocks = ceil(static_cast<float>(arrayLength) / threads);
-
-    return {dim3(blocks), dim3(threads)};
-}
-
-// CUDA kernel configurator for parallel computing on 2D blocks and grids
-__host__ pair<dim3, dim3> configurator2D(void *kernelFun, int arrayWidth, int arrayHeight) {
+__host__ std::pair<dim3, dim3> configurator1D(void *kernelFun, int arrayLength) {
     int blockSize;
     int minGridSize;
 
     cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, kernelFun);
 
-    int threadsPerDimension = static_cast<int>(sqrt(blockSize));
+    int threadsPerDimension = blockSize;
+    int blocksPerDimension = ceil(arrayLength / threadsPerDimension);
 
-    dim3 threads(threadsPerDimension, threadsPerDimension);
-    dim3 blocks(ceil(static_cast<float>(arrayWidth) / threads.x),
-                ceil(static_cast<float>(arrayHeight) / threads.y));
+    dim3 configBlocks(threadsPerDimension);
+    dim3 configGrids(blocksPerDimension);
 
-    return {blocks, threads};
+    return {configGrids, configBlocks};
 }
 
-// CUDA kernel configurator for parallel computing on 3D blocks and grids
-__host__ pair<dim3, dim3> configurator3D(void *kernelFun, int arrayWidth, int arrayHeight,
-                                         int arrayDepth) {
+// CUDA kernel configurator for parallel computing on 2D blocks and grids
+__host__ std::pair<dim3, dim3> configurator2D(void *kernelFun, int arrayWidth, int arrayHeight) {
     int blockSize;
     int minGridSize;
 
-    // Get the potential block size for maximum occupancy
-    cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize,
-                                       kernelFun); // Use CUDA occupancy calculator
+    cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, kernelFun);
 
-    int threadsPerDimension = static_cast<int>(cbrt(blockSize));
+    int threadsPerDimension = sqrt(blockSize);
 
-    dim3 threads(threadsPerDimension, threadsPerDimension, threadsPerDimension);
-    dim3 blocks(ceil(static_cast<float>(arrayWidth) / threads.x),
-                ceil(static_cast<float>(arrayHeight) / threads.y),
-                ceil(static_cast<float>(arrayDepth) / threads.z));
+    dim3 configBlocks(threadsPerDimension, threadsPerDimension);
+    dim3 configGrids(ceil(arrayWidth / configBlocks.x), ceil(arrayHeight / configBlocks.y));
+
+    return {configGrids, configBlocks};
+}
+
+// CUDA kernel configurator for parallel computing on 3D blocks and grids
+__host__ std::pair<dim3, dim3> configurator3D(void *kernelFun, int arrayWidth, int arrayHeight,
+                                              int arrayDepth) {
+    int blockSize;
+    int minGridSize;
+
+    cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, kernelFun);
+
+    int threadsPerDimension = cbrt(blockSize);
+
+    dim3 configBlocks(threadsPerDimension, threadsPerDimension, threadsPerDimension);
+    dim3 configGrids(ceil(arrayWidth / configBlocks.x), ceil(arrayHeight / configBlocks.y),
+                     ceil(arrayDepth / configBlocks.z));
 
     return {blocks, threads};
 }

453c84525b158ee81871fb3cf0d43502879790ac
huiyuxie <huiyuxie.sde@gmail.com>
2024-08-07 16:21:20 -1000

    Add copy to and from functions

--- src/solvers/common.jl
+++ src/solvers/common.jl
@@ -1,4 +1,5 @@
 # Define common functions for all solvers
+# fixme: Dispatch different functions for Flaot64 and Float32 to improve performance, maybe exists a better way for Float32
 
 # Copy matrices from host to device (Float64)
 function copy_to_device!(du::PtrArray{Float64}, u::PtrArray{Float64})
@@ -9,9 +10,10 @@ function copy_to_device!(du::PtrArray{Float64}, u::PtrArray{Float64})
 end
 
 # Copy matrices from device to host (Float64)
-function copy_to_host!(du::PtrArray{Float64}, u::PtrArray{Float64})
-    du = Array(du)
-    u = Array(u)
+function copy_to_host!(du::CuArray{Float64}, u::CuArray{Float64})
+    # fixme: maybe direct PtrArray to CuArray conversion is possible
+    du = PtrArray(Array(du))
+    u = PtrArray(Array(u))
 
     return (du, u)
 end

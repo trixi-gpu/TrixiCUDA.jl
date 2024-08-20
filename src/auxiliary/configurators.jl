2ebe44ca6866bb200f6608aeb8fe1da732866f00
huiyuxie <huiyuxie.sde@gmail.com>
2024-08-20 12:14:49 -1000

    Apply new formatter

--- src/auxiliary/configurators.jl
+++ src/auxiliary/configurators.jl
@@ -2,35 +2,31 @@
 # threads and blocks to be used in the kernel, which optimizes the use of GPU resources.
 
 # Kernel configurator for 1D CUDA array
-function configurator_1d(kernel::HostKernel, array::CuArray{<:Any,1})
+function configurator_1d(kernel::HostKernel, array::CuArray{<:Any, 1})
     config = launch_configuration(kernel.fun)
 
     threads = min(length(array), config.threads)
     blocks = cld(length(array), threads)
 
-    return (threads=threads, blocks=blocks)
+    return (threads = threads, blocks = blocks)
 end
 
 # Kernel configurator for 2D CUDA array 
-function configurator_2d(kernel::HostKernel, array::CuArray{<:Any,2})
+function configurator_2d(kernel::HostKernel, array::CuArray{<:Any, 2})
     config = launch_configuration(kernel.fun)
 
-    threads = Tuple(
-        fill(Int(floor((min(maximum(size(array)), config.threads))^(1 / 2))), 2)
-    )
+    threads = Tuple(fill(Int(floor((min(maximum(size(array)), config.threads))^(1 / 2))), 2))
     blocks = map(cld, size(array), threads)
 
-    return (threads=threads, blocks=blocks)
+    return (threads = threads, blocks = blocks)
 end
 
 # Kernel configurator for 3D CUDA array
-function configurator_3d(kernel::HostKernel, array::CuArray{<:Any,3})
+function configurator_3d(kernel::HostKernel, array::CuArray{<:Any, 3})
     config = launch_configuration(kernel.fun)
 
-    threads = Tuple(
-        fill(Int(floor((min(maximum(size(array)), config.threads))^(1 / 3))), 3)
-    )
+    threads = Tuple(fill(Int(floor((min(maximum(size(array)), config.threads))^(1 / 3))), 3))
     blocks = map(cld, size(array), threads)
 
-    return (threads=threads, blocks=blocks)
+    return (threads = threads, blocks = blocks)
 end
--- src/auxiliary/methods.jl
+++ src/auxiliary/methods.jl
@@ -2,18 +2,22 @@
 
 # Ref: `get_node_vars(u, equations, solver::DG, indices...)` in Trixi.jl
 @inline function get_node_vars(u, equations, indices...)
-    return SVector(ntuple(@inline(v -> u[v, indices...]), Val(nvariables(equations))))
+    return SVector(ntuple(@inline(v->u[v, indices...]),
+                          Val(nvariables(equations))))
 end
 
 # Ref: `get_node_coords(x, equations, solver::DG, indices...)` in Trixi.jl
 @inline function get_node_coords(x, equations, indices...)
-    return SVector(ntuple(@inline(idx -> x[idx, indices...]), Val(ndims(equations))))
+    return SVector(ntuple(@inline(idx->x[idx, indices...]),
+                          Val(ndims(equations))))
 end
 
 # Ref: `get_surface_node_vars(u, equations, solver::DG, indices...)` in Trixi.jl
 @inline function get_surface_node_vars(u, equations, indices...)
-    u_ll = SVector(ntuple(@inline(v -> u[1, v, indices...]), Val(nvariables(equations))))
-    u_rr = SVector(ntuple(@inline(v -> u[2, v, indices...]), Val(nvariables(equations))))
+    u_ll = SVector(ntuple(@inline(v->u[1, v, indices...]),
+                          Val(nvariables(equations))))
+    u_rr = SVector(ntuple(@inline(v->u[2, v, indices...]),
+                          Val(nvariables(equations))))
 
     return u_ll, u_rr
 end

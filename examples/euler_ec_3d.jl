f7e45f0f581b7ff6b71f6883c17e7445ab620692
huiyuxie <huiyuxie.sde@gmail.com>
2024-08-20 12:09:19 -1000

    Format euler ec

--- examples/euler_ec_3d.jl
+++ examples/euler_ec_3d.jl
@@ -1,24 +1,29 @@
 using Trixi, TrixiGPU
 using OrdinaryDiffEq
 
+# The example is taken from the Trixi.jl
+
+###############################################################################
+# semidiscretization of the compressible Euler equations
+
 equations = CompressibleEulerEquations3D(1.4)
 
 initial_condition = initial_condition_weak_blast_wave
 
 volume_flux = flux_ranocha
-solver = DGSEM(;
-    polydeg=3,
-    surface_flux=flux_ranocha,
-    volume_integral=VolumeIntegralFluxDifferencing(volume_flux),
-)
+solver = DGSEM(polydeg = 3, surface_flux = flux_ranocha,
+               volume_integral = VolumeIntegralFluxDifferencing(volume_flux))
 
 coordinates_min = (-2.0, -2.0, -2.0)
 coordinates_max = (2.0, 2.0, 2.0)
-mesh = TreeMesh(
-    coordinates_min, coordinates_max; initial_refinement_level=3, n_cells_max=100_000
-)
+mesh = TreeMesh(coordinates_min, coordinates_max,
+                initial_refinement_level = 3,
+                n_cells_max = 100_000)
 
 semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver)
+
+###############################################################################
+# ODE solvers, callbacks etc.
 
 tspan = (0.0, 0.4)
 ode = semidiscretize_gpu(semi, tspan) # from TrixiGPU.jl
@@ -26,28 +31,26 @@ ode = semidiscretize_gpu(semi, tspan) # from TrixiGPU.jl
 summary_callback = SummaryCallback()
 
 analysis_interval = 100
-analysis_callback = AnalysisCallback(semi; interval=analysis_interval)
+analysis_callback = AnalysisCallback(semi, interval = analysis_interval)
 
-alive_callback = AliveCallback(; analysis_interval=analysis_interval)
+alive_callback = AliveCallback(analysis_interval = analysis_interval)
 
-save_solution = SaveSolutionCallback(;
-    interval=100,
-    save_initial_solution=true,
-    save_final_solution=true,
-    solution_variables=cons2prim,
-)
+save_solution = SaveSolutionCallback(interval = 100,
+                                     save_initial_solution = true,
+                                     save_final_solution = true,
+                                     solution_variables = cons2prim)
 
-stepsize_callback = StepsizeCallback(; cfl=1.3)
+stepsize_callback = StepsizeCallback(cfl = 1.3)
 
-callbacks = CallbackSet(
-    summary_callback, analysis_callback, alive_callback, save_solution, stepsize_callback
-)
+callbacks = CallbackSet(summary_callback,
+                        analysis_callback, alive_callback,
+                        save_solution,
+                        stepsize_callback)
 
-sol = solve(
-    ode,
-    CarpenterKennedy2N54(; williamson_condition=false);
-    dt=1.0,
-    save_everystep=false,
-    callback=callbacks,
-);
-summary_callback()
+###############################################################################
+# run the simulation
+
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition = false),
+            dt = 1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
+            save_everystep = false, callback = callbacks);
+summary_callback() # print the timer summary

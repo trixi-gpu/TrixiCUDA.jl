76cc9188cca03c889e022e1eddda39da59f63f74
Huiyu Xie <huiyuxie.sde@gmail.com>
2024-09-20 08:06:01 -0400

    Enable documentation for package (#50)
    * Start
    
    * Fix error

---
+++ docs/make.jl
@@ -0,0 +1,17 @@
+# TrixiCUDA.jl is not accessible through Julia's LOAD_PATH currently
+push!(LOAD_PATH, "../src/")
+
+using Documenter
+using TrixiCUDA
+
+# DocMeta.setdocmeta!(TrixiCUDA, :DocTestSetup, :(using TrixiCUDA); recursive = true)
+
+makedocs(sitename = "TrixiCUDA.jl",
+         #  pages = [
+         #      "Home" => "index.md"
+         #  ],
+         format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"))
+
+deploydocs(repo = "github.com/trixi-gpu/TrixiCUDA.jl",
+           devbranch = "main",
+           push_preview = true)

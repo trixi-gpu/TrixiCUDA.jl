67f68e7086df6ee409eecf5402f27c5a5501306a
huiyuxie <huiyuxie.sde@gmail.com>
2024-09-21 20:11:53 -1000

    Add tutorials

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,9 @@ DocMeta.setdocmeta!(TrixiCUDA, :DocTestSetup, :(using TrixiCUDA); recursive = tr
 
 makedocs(sitename = "TrixiCUDA.jl",
          pages = [
-             "Home" => "index.md"
+             "Home" => "index.md",
+             "Tutorial 1" => "aws_gpu_setup.md",
+             "Tutorial 2" => "nsys_profiling.md"
          ],
          format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"))
 

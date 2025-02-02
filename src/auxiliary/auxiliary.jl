81581b6394b87c708159e5e4b0a21da71e98ae51
huiyuxie <huiyuxie.sde@gmail.com>
2025-02-02 12:06:09 -1000

    Start

--- src/auxiliary/auxiliary.jl
+++ src/auxiliary/auxiliary.jl
@@ -1,17 +1,17 @@
 include("configurators.jl")
 include("stable.jl")
 
-# Initialize the device properties
+# Initialize the device 
 function init_device()
+    # Get the device properties
     try
-        # Consider single GPU for now
         # TODO: Consider multiple GPUs later
         device = CUDA.device()
 
-        # Get the device properties
         global MULTIPROCESSOR_COUNT = CUDA.attribute(device, CUDA.CU_DEVICE_ATTRIBUTE_MULTIPROCESSOR_COUNT)
         global MAX_THREADS_PER_BLOCK = CUDA.attribute(device, CUDA.CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_BLOCK)
         global MAX_SHARED_MEMORY_PER_BLOCK = CUDA.attribute(device, CUDA.CU_DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK)
+        # Catch the errors
     catch e
         # Handle the errors
         if e isa CUDA.CuError
@@ -25,5 +25,21 @@ function init_device()
         global MULTIPROCESSOR_COUNT = 0
         global MAX_THREADS_PER_BLOCK = 0
         global MAX_SHARED_MEMORY_PER_BLOCK = 0
+    end
+
+    # Initialize the CUDA streams
+    try
+        global STREAM1 = CUDA.CuStream()
+        global STREAM2 = CUDA.CuStream()
+        # Catch the errors
+    catch e
+        println("Error initializing CUDA streams: ", e)
+        println("Ensure there are enough GPU resources for stream creation.")
+
+        # Fallback to default stream
+        global STREAM1 = CUDA.stream()
+        global STREAM2 = CUDA.stream()
+
+        println("Using the default stream as a fallback.")
     end
 end

bbb485c74f192df3230fa7a1ee02e0d334611659
huiyuxie <huiyuxie.sde@gmail.com>
2025-02-04 10:35:18 -1000

    Start

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,9 +7,9 @@ using CUDA
 CUDA.allowscalar(true)
 
 @testset "TrixiCUDA.jl" begin
-    # include("./tree_dgsem_1d/tree_dgsem_1d.jl")
-    # include("./tree_dgsem_2d/tree_dgsem_2d.jl")
-    # include("./tree_dgsem_3d/tree_dgsem_3d.jl")
+    include("./tree_dgsem_1d/tree_dgsem_1d.jl")
+    include("./tree_dgsem_2d/tree_dgsem_2d.jl")
+    include("./tree_dgsem_3d/tree_dgsem_3d.jl")
 end
 
 end # module

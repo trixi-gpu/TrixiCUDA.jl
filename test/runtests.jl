ca567fed833e4e98a34012724bc9e0033bb601d2
huiyuxie <huiyuxie.sde@gmail.com>
2025-02-04 16:17:58 -1000

    Final test

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,17 +15,15 @@ CUDA.allowscalar(true)
     @info "Max threads per block: $(TrixiCUDA.MAX_THREADS_PER_BLOCK)"
     @info "Max shared memory per block: $(TrixiCUDA.MAX_SHARED_MEMORY_PER_BLOCK)"
 
-    # @info "Starting TrixiCUDA test suite..."
+    @info "Starting TrixiCUDA test suite..."
 
-    # for (dim, path) in [("1D", "./tree_dgsem_1d/tree_dgsem_1d.jl"),
-    #     ("2D", "./tree_dgsem_2d/tree_dgsem_2d.jl"),
-    #     ("3D", "./tree_dgsem_3d/tree_dgsem_3d.jl")]
-    #     @info "Running $dim DGSEM tree tests..."
-    #     @time include(path)
-    #     @info "Completed $dim DGSEM tree tests"
-    # end
+    for (dim, path) in [("1D", "./tree_dgsem_1d/tree_dgsem_1d.jl"),
+        ("2D", "./tree_dgsem_2d/tree_dgsem_2d.jl"),
+        ("3D", "./tree_dgsem_3d/tree_dgsem_3d.jl")]
+        @info "Running $dim DGSEM tree tests..."
+        @time include(path)
+        @info "Completed $dim DGSEM tree tests"
+    end
 
-    # @info "All TrixiCUDA tests completed successfully"
-
-    include("../test.jl")
+    @info "All TrixiCUDA tests completed successfully"
 end

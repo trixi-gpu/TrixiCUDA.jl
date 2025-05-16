a25264abd5f45e0a412edcb08787df3c1723abee
huiyuxie <huiyuxie.sde@gmail.com>
2025-05-16 15:53:19 -0400

    Fix examples

--- src/solvers/indicators_2d.jl
+++ src/solvers/indicators_2d.jl
@@ -1,1 +1,17 @@
+# Currently hold cache for indicators on CPU
+function create_cache(::Type{IndicatorHennemannGassner},
+                      equations::AbstractEquations{2}, basis::LobattoLegendreBasisGPU)
+    alpha = Vector{real(basis)}()
+    alpha_tmp = similar(alpha)
 
+    A = Array{real(basis), ndims(equations)}
+
+    # Single indicator and modal arrays (single thread)
+    # Note: This can be optimized by moving to GPU but need to be careful about
+    # its actual performance.
+    indicator_threaded = [A(undef, nnodes(basis), nnodes(basis))]
+    modal_threaded = [A(undef, nnodes(basis), nnodes(basis))]
+    modal_tmp1_threaded = [A(undef, nnodes(basis), nnodes(basis))]
+
+    return (; alpha, alpha_tmp, indicator_threaded, modal_threaded, modal_tmp1_threaded)
+end
--- src/solvers/indicators_3d.jl
+++ src/solvers/indicators_3d.jl
@@ -1,1 +1,20 @@
 
+# Currently hold cache for indicators on CPU
+function create_cache(::Type{IndicatorHennemannGassner},
+                      equations::AbstractEquations{3}, basis::LobattoLegendreBasisGPU)
+    alpha = Vector{real(basis)}()
+    alpha_tmp = similar(alpha)
+
+    A = Array{real(basis), ndims(equations)}
+
+    # Single indicator and modal arrays (single thread)
+    # Note: This can be optimized by moving to GPU but need to be careful about
+    # its actual performance.
+    indicator_threaded = [A(undef, nnodes(basis), nnodes(basis), nnodes(basis))]
+    modal_threaded = [A(undef, nnodes(basis), nnodes(basis), nnodes(basis))]
+    modal_tmp1_threaded = [A(undef, nnodes(basis), nnodes(basis), nnodes(basis))]
+    modal_tmp2_threaded = [A(undef, nnodes(basis), nnodes(basis), nnodes(basis))]
+
+    return (; alpha, alpha_tmp, indicator_threaded, modal_threaded, modal_tmp1_threaded,
+            modal_tmp2_threaded)
+end

75597806c6c98897efaa8ead521638d18432374b
huiyuxie <huiyuxie.sde@gmail.com>
2025-06-18 20:40:16 -0400

    Add new timer

--- src/auxiliary/auxiliary.jl
+++ src/auxiliary/auxiliary.jl
@@ -1,5 +1,5 @@
 include("configurators.jl")
-include("stable.jl")
+include("timer.jl")
 
 # Initialize the device properties
 function init_device()
@@ -26,4 +26,56 @@ function init_device()
         global MAX_THREADS_PER_BLOCK = 0
         global MAX_SHARED_MEMORY_PER_BLOCK = 0
     end
+end
+
+# Similar to `get_node_vars(u, equations, solver::DG, indices...)` in Trixi.jl, but we 
+# avoid using `solver::DG` as it is not stable on GPU.
+# Note that it also serves a way to allocate array per thread in GPU kernels.
+@inline function get_node_vars(u, equations, indices...)
+    return SVector(ntuple(@inline(v->u[v, indices...]), Val(nvariables(equations))))
+end
+
+# Similar to `get_node_coords(x, equations, solver::DG, indices...)` in Trixi.jl, but we
+# avoid using `solver::DG` as it is not stable on GPU.
+# Note that it also serves a way to allocate array per thread in GPU kernels.
+@inline function get_node_coords(x, equations, indices...)
+    return SVector(ntuple(@inline(idx->x[idx, indices...]), Val(ndims(equations))))
+end
+
+# Similar to `get_surface_node_vars(u, equations, solver::DG, indices...)` in Trixi.jl,
+# but we avoid using `solver::DG` as it is not stable on GPU.
+# Note that it also serves a way to allocate array per thread in GPU kernels.
+@inline function get_surface_node_vars(u, equations, indices...)
+    u_ll = SVector(ntuple(@inline(v->u[1, v, indices...]), Val(nvariables(equations))))
+    u_rr = SVector(ntuple(@inline(v->u[2, v, indices...]), Val(nvariables(equations))))
+
+    return u_ll, u_rr
+end
+
+# To be used outside GPU kernels, accepts both GPU and CPU arrays
+@inline function get_node_vars_view(u::Union{CuArray, Array}, equations, solver::DG, indices...)
+    return @view u[:, indices...]
+end
+
+# Helper function for checking `cache.mortars`
+@inline function check_cache_mortars(cache)
+    if iszero(length(cache.mortars.orientations))
+        return False()
+    else
+        return True()
+    end
+end
+
+# Callable function to replace the `boundary_condition_periodic` from Trixi.jl
+@inline function boundary_condition_periodic_callable(u_inner, orientation,
+                                                      direction, x, t, surface_flux, equations)
+    return nothing
+end
+
+# Replace the `boundary_condition_periodic` from Trixi.jl with a callable one
+function replace_boundary_conditions(boundary_conditions::NamedTuple)
+    keys_ = keys(boundary_conditions)
+    values_ = (func == boundary_condition_periodic ? boundary_condition_periodic_callable : func
+               for func in values(boundary_conditions))
+    return NamedTuple{keys_}(values_)
 end
---
+++ src/auxiliary/timer.jl
@@ -0,0 +1,74 @@
+function pretty_time(t::Float64) # Float32?
+    if t >= 1
+        return @sprintf("%.2f s", t)
+    elseif t >= 1e-3
+        return @sprintf("%.2f ms", t*1e3)
+    elseif t >= 1e-6
+        return @sprintf("%.2f µs", t*1e6)
+    else
+        return @sprintf("%.2f ns", t*1e9)
+    end
+end
+
+function pretty_bytes(bytes::Int64) # Int32?
+    bytes, mb = Base.prettyprint_getunits(bytes, length(Base._mem_units), Int64(1024))
+    unit = Base._mem_units[mb]
+    if mb == 1 # less than KiB
+        suffix = (bytes == 0 || bytes == 1) ? "" : "s" # 0 byte or 1 byte
+        return @sprintf("%d byte%s", bytes, suffix)
+    else # KiB, MiB, GiB, TiB, PiB
+        return @sprintf("%.2f %s", bytes, unit)
+    end
+end
+
+# Timer with device synchronization
+macro timer(args...)
+    arg_list = collect(args)
+
+    # Determine if a description string was provided
+    if length(arg_list) == 2 && arg_list[1] isa String
+        desc = arg_list[1]
+        expr = arg_list[2]
+    elseif length(arg_list) == 1
+        desc = 0
+        expr = arg_list[1]
+    else
+        error("@timer usage error: use either @timer \"desc\" expr or @timer expr")
+    end
+
+    quote
+        if $(esc(desc)) != 0
+            @info $(esc(desc))
+        end
+
+        local val, cpu_time,
+        cpu_alloc_size, cpu_gc_time, cpu_mem_stats,
+        gpu_alloc_size, gpu_mem_time, gpu_mem_stats = CUDA.@timed $(esc(expr)) # synchronize in CUDA.@timed
+
+        local cpu_alloc_count = Base.gc_alloc_count(cpu_mem_stats)
+        local gpu_alloc_count = gpu_mem_stats.alloc_count
+
+        println("Total time: ", pretty_time(cpu_time))
+
+        # Loop over CPU and GPU data
+        for (type, gctime, memtime, bytes, allocs) in (("CPU", cpu_gc_time, 0, cpu_alloc_size, cpu_alloc_count),
+                                                       ("GPU", 0, gpu_mem_time, gpu_alloc_size, gpu_alloc_count))
+            allocs, ma = Base.prettyprint_getunits(allocs, length(Base._cnt_units), Int64(1000))
+            if ma == 1 # less than K
+                suffix = (allocs == 0 || allocs == 1) ? "" : "s" # 0 alloc or 1 alloc
+                @printf("%s allocation: %d%s alloc%s, ", type, allocs, Base._cnt_units[ma], suffix)
+            else # K, M, G, T, P
+                @printf("%s allocation: %.2f%s allocs, ", type, allocs, Base._cnt_units[ma])
+            end
+            print(pretty_bytes(bytes))
+            if gctime > 0
+                Printf.@printf(", %.2f%% gc time", 100 * gctime/cpu_time)
+            end
+            if memtime > 0
+                Printf.@printf(", %.2f%% memmgmt time", 100 * memtime/cpu_time)
+            end
+            println()
+        end
+        println()
+    end
+end
